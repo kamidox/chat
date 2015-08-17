cdcd20fb49ec92c22e3e2dc6ada181db2dfe92d9
Joey Huang <kamidox@gmail.com>
2015-08-17 14:07:28 +0800

    connection limit

--- chatclient.py
+++ chatclient.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime
 from tornado import httpclient, gen, ioloop, escape, queues
 
-WORKER_NUM = 1000
+MAX_CLIENTS = 500
 HOST = 'http://localhost:8888'
 
 def timestamp():
@@ -15,8 +15,22 @@ def log(info):
 
 @gen.coroutine
 def poll(idx):
-	"""poll messages from /a/message/updates by using long-poll"""
-	client = httpclient.AsyncHTTPClient(max_clients=WORKER_NUM)
+	"""poll messages from /a/message/updates by using long-poll
+
+	In windows, tornado use select() to implement io loop, for windows 7 professional, 
+	it's limitation is 512.
+
+	Tornado should run on any Unix-like platform, although for the best performance 
+	and scalability only Linux (with epoll) and BSD (with kqueue) are recommended 
+	for production deployment (even though Mac OS X is derived from BSD and supports 
+	kqueue, its networking performance is generally poor so it is recommended only 
+	for development use). Tornado will also run on Windows, although this configuration 
+	is not officially supported and is recommended only for development use.
+
+	Platforms: http://www.tornadoweb.org/en/stable/index.html
+	Running and deploying: http://www.tornadoweb.org/en/stable/guide/running.html
+	"""
+	client = httpclient.AsyncHTTPClient(max_clients=MAX_CLIENTS)
 	log('worker#%05d: polling from %s' % (idx, HOST))
 	body = {'_xsrf': 'undefined'}
 	rsp = yield client.fetch(HOST + "/a/message/updates", 
@@ -38,15 +52,15 @@ def worker(idx):
 def main():
 	"""main"""
 	q = queues.Queue()
-	q.put(WORKER_NUM)
+	q.put(MAX_CLIENTS)
 
-	for i in range(WORKER_NUM):
+	for i in range(MAX_CLIENTS):
 		worker(i)
 
 	yield q.join()
 
 if __name__ == '__main__':
-	log('%d workers is polling. press Control + C to quit.' % WORKER_NUM)
+	log('%d workers is polling. press Control + C to quit.' % MAX_CLIENTS)
 	try:
 		ioloop.IOLoop.current().run_sync(main)
 	except KeyboardInterrupt as e:
